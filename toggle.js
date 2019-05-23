24025e494a84225ac07176187b94f561e6ca5731
Kevin Sims <kevinsims@Kevins-MacBook-Pro.local>
2019-05-23 14:48:30 -0700

    changes

--- toggle.js
+++ toggle.js
@@ -3,7 +3,9 @@ class Toggle {
         this.e = e;
         this.toggle = this.e.querySelector('.toggle');
         this.paragraph = this.e.querySelector('p');
-        this.toggle.addEventListener('click', () => {this.select()})
+        this.toggle.addEventListener('click', () => {this.select()});
+        this.toggle.addEventListener('mouseover', () => {this.mouseover()});
+        this.toggle.addEventListener('mouseleave', () => {this.mouseleave()})
 
     }
     select() {
@@ -13,6 +15,14 @@ class Toggle {
         } else {
             this.paragraph.style.visibility = "visible";
         }
+    }
+    mouseover() {
+        this.toggle.style.color = "green"
+        this.toggle.style.background = "white"
+    }
+    mouseleave() {
+        this.toggle.style.color = "white"
+        this.toggle.style.background = "green"
     }
 }
 
